b325fd14fad3636322b7b537c4db2d663c7e6783
Duncan Murray <djmurray@acutesoftware.com.au>
2016-04-03 23:30:37 +0930

    tst.py adds content type for output

---
+++ test/tst.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+print('Content-Type: text/html; charset=utf-8\n')
+print("<H1>AIKIF Tools: Date Calculator</H1>\n")
+
+import sys
+import os
+
+
+def main():
+    print("<BR>\n<H3>Enter a date</h3>\n")
+
+    
+if __name__ == "__main__":
+    main()
+
+    
+
