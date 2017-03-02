9f441022fa68b0c93162ed59c97391fce6ddb7f0
Duncan Murray <djmurray@gmail.com>
2017-03-02 22:46:09 +1030

    working on database connection

--- webapp/aikif.py
+++ webapp/aikif.py
@@ -14,8 +14,19 @@ import web_utils as web
 from flask import Flask
 from flask import request
 from flask import render_template
-    
+from flask import session
+from flask import g
+from flask import redirect
+from flask import url_for
+from flask import abort
+from flask import flash   
+   
+   
+   
+import sqlite3
+   
 app = Flask(__name__)
+
 menu = [
     ['/',        'Home',     'This is the admin web interface for AIKIF (Artificial Intelligence Knowledge Information Framework)'],
     ['/data',    'Data',     'Shows the available data sets for AIKIF'],
@@ -25,7 +36,66 @@ menu = [
     ['/about',   'About',    'About AIKIF and author contact']
     ]
 
+###### DATABASE #####
 
+
+
+app.config.from_object(__name__)
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='12345yesreally'
+))
+
+#os.environ['FLASK_APP'] = 'aikif.py'
+os.environ['FLASK_APP'] = "aikif.py"
+#app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')    
+    
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+  
+
+
+# Now, it is possible to create a database with the flask script:
+#flask initdb
+#Initialized the database.
+  
+initdb_command()  
+  
+    
 ###################### HELPER FUNCTIONS#################
 def start_server():
     if AIKIF_WEB_VERSION == "DEV":
