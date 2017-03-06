70e99a040613f4cdcfdfd075a2abbd10795ec97b
Duncan Murray <djmurray@gmail.com>
2017-03-07 00:28:55 +1030

    added core_facts and core_events to schema.py - created tables in db successfully

--- webapp/schema.sql
+++ webapp/schema.sql
@@ -4,3 +4,31 @@ create table entries (
   title text not null,
   'text' text not null
 );
+
+---------------------------------------------
+-- CREATE Fact Table - CORE_EVENTS
+---------------------------------------------
+DROP TABLE IF EXISTS CORE_EVENTS;
+CREATE TABLE CORE_EVENTS (
+id VARCHAR2(200), 
+ name VARCHAR2(200), 
+ key VARCHAR2(200), 
+ value VARCHAR2(200), 
+ UPDATE_DATE DATE 
+);
+
+CREATE INDEX ndx_CORE_EVENTS ON CORE_EVENTS (id,name,key,value );
+
+---------------------------------------------
+-- CREATE Fact Table - CORE_FACTS
+---------------------------------------------
+DROP TABLE IF EXISTS CORE_FACTS;
+CREATE TABLE CORE_FACTS (
+id VARCHAR2(200), 
+ name VARCHAR2(200), 
+ key VARCHAR2(200), 
+ value VARCHAR2(200), 
+ UPDATE_DATE DATE 
+);
+
+CREATE INDEX ndx_CORE_FACTS ON CORE_FACTS (id,name,key,value );
