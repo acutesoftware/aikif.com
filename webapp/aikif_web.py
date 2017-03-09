0badc939dc98ec34694114cb914d85c56c1ff72d
Duncan Murray <djmurray@gmail.com>
2017-03-09 21:55:32 +1030

    login and logout functions - initial version of user management

--- webapp/aikif_web.py
+++ webapp/aikif_web.py
@@ -104,16 +104,47 @@ def start_server():
     if AIKIF_WEB_VERSION == "DEV":
         print("WARNING - DEBUG MODE ACTIVE")
         app.debug = True # TURN THIS OFF IN PRODUCTION
-    app.run()
+        app.run()
+    else:
+        app.run(threaded=True, host='0.0.0.0', port=5000)    
 
 ###################### ROUTING #########################
 
+is_authenticated = False
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('page_home'))
+
+
 @app.route("/")
 def page_home():
+    #user = g.user
     return render_template('index.html',
                            footer=get_footer())
 
 
+                           
+                           
+                           
+                           
+                           
 
 @app.route('/', methods=['POST'])
 def search_post():
