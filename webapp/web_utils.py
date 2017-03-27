22143f68bb0fb95e975c332350d271907bb61347
Duncan Murray <djmurray@gmail.com>
2017-03-27 22:28:34 +1030

    removed old HTML functions prior to flask templates

--- webapp/web_utils.py
+++ webapp/web_utils.py
@@ -10,26 +10,6 @@ import sys
 import csv
 
 
-def list2html(lst):
-    """ 
-    convert a list to html using table formatting 
-    """
-    txt = '<TABLE width=100% border=0>'
-    for l in lst:
-        txt += '<TR>\n'
-        if type(l) is str:
-            txt+= '<TD>' + l + '</TD>\n'
-        elif type(l) is list:
-            txt+= '<TD>'
-            for i in l:
-                txt += i + ', '
-            txt+= '</TD>'
-        else:
-            txt+= '<TD>' + str(l) + '</TD>\n'
-        txt += '</TR>\n'
-    txt += '</TABLE><BR>\n'
-    return txt
-
 def GetFileList(rootPaths, lstXtn, shortNameOnly='Y'):
     """ 
     builds a list of files and returns as a list 
@@ -51,25 +31,6 @@ def GetFileList(rootPaths, lstXtn, shortNameOnly='Y'):
                             opFileList.append(filename)
                         
     return sorted(opFileList)
-
-  
-def filelist2html_div(lst, fldr):
-    """ 
-    convert a list to html using DIV formatting 
-    """
-    txt = ''
-    for l in lst:
-        if type(l) is str:
-            txt+= ' ' + link_file(l, fldr) + '\n'
-        elif type(l) is list:
-            for i in l:
-                txt+= link_file(i, fldr) + ', '
-            txt+= '\n'
-        else:
-            txt+= ' ' + str(l) + '\n'
-        txt += '<BR>\n'
-    txt += '\n'
-    return txt
 
 def build_search_form():
     """ 
@@ -166,86 +127,6 @@ def link_file(f, fldr):
             return '<a href="data/' + f + '">' + f + '</a>'
     else:
         return f
-    
-def dict_to_htmlrow(d):
-    """
-    converts a dictionary to a HTML table row
-    """
-    res = "<TR>\n"
-    for k, v in d.items():
-        if type(v) == str:
-            res = res + '<TD><p>' + k + ':</p></TD><TD><p>' + v + '</p></TD>'
-        else:
-            res = res + '<TD><p>' + k + ':</p></TD><TD><p>' + str(v) + '</p></TD>'
-    res += '</TR>\n'
-    return res
-
-def read_csv_to_html_table(csvFile, hasHeader='N', maxrows=200):
-    """
-    reads a CSV file and converts it to HTML
-    """
-    txt = '<table class="as-table as-table-zebra as-table-horizontal">'
-    with open(csvFile, "r") as f:  # 
-        numRows = 1
-        for row in f:
-            if hasHeader == 'Y':
-                if numRows == 1:
-                    td_begin = '<TH>'
-                    td_end = '</TH>'
-                else:
-                    td_begin = '<TD>'
-                    td_end = '</TD>'
-            else:
-                td_begin = '<TD>'
-                td_end = '</TD>'
-            cols = row.split(',')
-            numRows += 1
-
-            txt += "<TR>"
-            for col in cols:
-                txt += td_begin
-                try:
-                    colString = col
-                except Exception:
-                    colString = '<font color=red>Error decoding column data</font>'
-                txt += colString.strip('"')	
-                txt += td_end
-            txt += "</TR>\n"
-            
-            if numRows > maxrows:
-                break
-        txt += "</TABLE>\n\n"
-    return txt
-    
-
-def read_csv_to_html_table_using_CSV_module_OLD(csvFile, hasHeader='N'):
-    txt = '<TABLE width=100% border=1>'
-    with open(csvFile, "rb") as csv_file:  # , encoding='utf-8'
-        numRows = 1
-        for row in csv.reader(csv_file, delimiter=','):
-            if hasHeader == 'Y':
-                if numRows == 1:
-                    td_begin = '<TH>'
-                    td_end = '</TH>'
-                else:
-                    td_begin = '<TD>'
-                    td_end = '</TD>'
-            else:
-                td_begin = '<TD>'
-                td_end = '</TD>'
-            numRows += 1
-
-            txt += "<TR>"
-            for col in row:
-                txt += td_begin
-                try:
-                    txt += col
-                except Exception:
-                    txt += 'Error'
-                txt += td_end
-            txt += "</TR>\n"
-        txt += "</TABLE>\n\n"
-    return txt
     
 def read_csv_to_html_list(csvFile):
     txt = ''
