6212a26c425e5272ee4f00a7f57b52ada39c9efa
Duncan Murray <djmurray@gmail.com>
2017-04-10 23:27:30 +0930

    script to build database uses appropriate schema based on DATABASE_DDL variable

--- webapp/rebuild_database.py
+++ webapp/rebuild_database.py
@@ -42,6 +42,8 @@ from aikif import core_data
 import web_utils as web
 from users import User
 
+DATABASE_DDL = 'schema_PostgreSQL.sql'
+DATABASE_DDL = 'schema_sqlite.sql'
 
 
 app = Flask(__name__)
@@ -101,7 +103,7 @@ def connect_db():
 
 def init_db():
     db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
+    with app.open_resource(DATABASE_DDL, mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
 
