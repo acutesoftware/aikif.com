7516323c6258497adce6e968ffd532b30ecc3f40
Duncan Murray <djmurray@gmail.com>
2017-05-03 22:19:18 +0930

    added parser for day with timeslots

--- webapp/events.py
+++ webapp/events.py
@@ -24,10 +24,10 @@ class Events(object):
         print(events)
         return events
 
-    def parse_events_for_web(self, events):
+    def parse_events_as_list(self, events):
         """
         takes a flat dataset of events and parses into dictionary
-        ready for web display
+        ready for list display
         {'date':'20170417', 'event':[
             {'time':'0900', 'details':'Initial Version'},
             {'time':'1100', 'details':'Update with database'},
@@ -61,3 +61,53 @@ class Events(object):
                     
             res.append(cur_event)
         return res
+        
+    def parse_events_as_day(self, events):
+        """
+        takes a flat dataset of events and parses into dictionary
+        ready for timeslot day display
+        {'date':'20170417', 'event':[
+            {'time':'0900', 'details':'Initial Version'},
+            {'time':'1000', 'details':''},
+            {'time':'1100', 'details':'Update with database'},
+            {'time':'1200', 'details':''},
+            {'time':'1300', 'details':''},
+            {'time':'1400', 'details':''},
+            ]
+        },
+        {'date':'20170422', 'event':[
+            {'time':'0900', 'details':''},
+            {'time':'1000', 'details':'bug fixes'},
+            {'time':'1100', 'details':''},
+            {'time':'1200', 'details':'more fixes'},
+            {'time':'1300', 'details':'calendar page'},
+            ]
+        },        
+        """
+        all_days = []
+        for e in events:
+            print('e = ', e)
+            all_days.append(e[0])
+        unique_days = list(set(all_days))
+        print('unique days = ', unique_days)
+        res = []
+        timeslots = ['0900','1000','1100','1200','1300','1400','1500','1600']
+        cur_event = {}
+        current_day = ''
+        for day in unique_days:  # what the hell - use something better
+            cur_event = {}
+            cur_event['date'] = day
+            cur_event['event'] = []
+            days_events = []
+            for timeslot in timeslots:
+                cur_details = ''
+                for e in events:
+                    if e[0] == day:
+                        if e[1] == timeslot:
+                            cur_details = e[2] + ' '
+                cur_event['event'].append( {'time': timeslot, 'details': cur_details})
+            days_events.append(cur_event)
+                    
+            res.append(cur_event)
+        return res        
+        
