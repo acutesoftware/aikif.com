5ade2b3f10db0421fd398c31e1b409fd86932ba9
Duncan Murray <djmurray@gmail.com>
2017-06-13 23:37:50 +0930

    starting tests for pages on aikif_web

--- test/test_web_aikif.py
+++ test/test_web_aikif.py
@@ -6,6 +6,12 @@ import unittest
 import sys
 import os
 
+root_folder = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.sep + ".." ) 
+pth = root_folder + os.sep + 'webapp'
+
+sys.path.append(pth)
+import aikif_web
+
 class aikif_web_Test(unittest.TestCase):
     
     def setUp(self):
@@ -14,9 +20,19 @@ class aikif_web_Test(unittest.TestCase):
     def tearDown(self):
         unittest.TestCase.tearDown(self)
  
-    def test_01_instantiate(self):
+    def test_02_page_home(self):
+        import aikif_web
+        res = aikif_web.page_home()
+        print(res)
 
-        self.assertEqual(5 , 5) 
+    def test_08_events(self):
+        # locks, so dont do this - aikif_web.start_server()
+        
+        ev = aikif_web.get_events()
+        print(ev)
+        self.assertEqual(len(ev) , 2) 
+    
+
         
 if __name__ == '__main__':
     unittest.main()
